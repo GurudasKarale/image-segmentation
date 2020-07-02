8051a3b95a27163458427afbd73ce598885a7ba8
MohitK22 <67753744+MohitK22@users.noreply.github.com>
2020-07-02 23:11:05 +0530

    Add files via upload

---
+++ isodataTresholding.py
@@ -0,0 +1,59 @@
+from PIL import Image
+import numpy  as np
+import matplotlib.pyplot as plt
+
+im = Image.open("C:/Users/Mohit K/Desktop/datasets/cameraman.tif")
+img=np.array(im)
+
+new=[]
+count=0
+for i in range(256):
+    for j in range(img.shape[1]):
+
+        for k in range(img.shape[0]):
+
+            if img[j][k]==i-1:
+                count+=1
+    new.append(count)
+    count=0
+print(new)
+
+summ=0
+for i in range(len(new)):
+    summ+=(i*new[i])
+
+median1=summ/65536
+
+def mean(a,th1,th2):
+    summ=0
+    count=0
+    for i in range(th1,th2):
+        summ+=(i*new[i])
+        count+=new[i]
+    m=summ/count
+    return m
+
+previousThreshold=new[1]
+nextThreshold=median1
+
+while(previousThreshold!=nextThreshold):
+    previousThreshold=nextThreshold
+    m1=mean(new,1,round(previousThreshold))
+    m2=mean(new,round(previousThreshold+1),len(new))
+    nextThreshold=(m1+m2)/2
+    nextThreshold=round(nextThreshold)
+
+print(nextThreshold)
+
+for i in range(256):
+    for j in range(256):
+
+        if img[i][j]>nextThreshold:
+            img[i][j]=255
+        else:
+            img[i][j]=0
+
+
+print(img)
+image2 = Image.fromarray(img)
+image2.show()
